3aea0387687282b7b61feb0cdcba2aa79f6e8612
geraldgsh <geraldgsh@hotmail.com>
2020-04-05 15:31:14 +0800

    added ui functionality

--- src/index.js
+++ src/index.js
@@ -12,30 +12,53 @@ const StarsDisplay = props => (
 );
 
 const PlayNumber = props => (
-  <button className="number" onClick={() => console.log('Num', props.number)}>
+  <button 
+  className="number"
+  style={{ backgroundColor: colors[props.status] }}
+  onClick={() => console.log('Num', props.number)}
+  >
     {props.number}
   </button>
 );
 
 const StarMatch = () => {
   const [stars, setStars] = useState(utils.random(1, 9));
+  const [availableNums, setAvailableNums] = useState(utils.range(1, 9));
+  const [candidateNums, setCanditateNums] = useState([]);
+
+  const candidatesAreWrong = utils.sum(candidateNums) > stars;
+
+  const numberStatus = (number) => {
+    if (!availableNums.includes(number)) {
+      return 'used';
+    }
+    if (candidateNums.includes(number)) {
+      return candidatesAreWrong ? 'wrong': 'candidate';
+    }
+    return 'available';
+  };
+
   return (
     <div className="game">
-      <div className="help">
-        Pick 1 or more numbers that sum to the number of stars
-      </div>
-      <div className="body">
-        <div className="left">
-          <StarsDisplay count={stars}/>
-        </div>
-        <div className="right">
-          {utils.range(1, 9).map(number =>
-            <PlayNumber key={number} number={number}/>
-          )}
-        </div>
-      </div>
-      <div className="timer">Time Remaining: 10</div>
+    <div className="help">
+      Pick 1 or more numbers that sum to the number of stars
     </div>
+    <div className="body">
+      <div className="left">
+      <StarsDisplay count={stars}/>
+    </div>
+    <div className="right">
+      {utils.range(1, 9).map(number =>
+        <PlayNumber 
+          key={number} 
+          status={numberStatus(number)}
+          number={number}
+        />
+      )}
+    </div>
+  </div>
+  <div className="timer">Time Remaining: 10</div>
+  </div>
   );
 };
 
