226795d8d3e42f1a12226c48127b6542bcc3b6cc
geraldgsh <geraldgsh@hotmail.com>
2020-04-05 15:30:28 +0800

    star display

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
+const StarsDisplay = props => (
+  <>
+    {utils.range(1, props.count).map(starId =>
+      <div key={starId} className="star" />
+    )}
+  </>
+);
+
+const PlayNumber = props => (
+  <button className="number" onClick={() => console.log('Num', props.number)}>
+    {props.number}
+  </button>
+);
 
 const StarMatch = () => {
   const [stars, setStars] = useState(utils.random(1, 9));
@@ -11,20 +24,18 @@ const StarMatch = () => {
       <div className="help">
         Pick 1 or more numbers that sum to the number of stars
       </div>
-    <div className="body">
-      <div className="left">
-        {utils.range(1, stars).map(starId =>
-            <div key={starId} className="star" />
-        )}
+      <div className="body">
+        <div className="left">
+          <StarsDisplay count={stars}/>
+        </div>
+        <div className="right">
+          {utils.range(1, 9).map(number =>
+            <PlayNumber key={number} number={number}/>
+          )}
+        </div>
       </div>
-      <div className="right">
-        {utils.range(1, 9).map(number =>
-          <button key={number} className="number">{number}</button>
-      )}
+      <div className="timer">Time Remaining: 10</div>
     </div>
-  </div>
-  <div className="timer">Time Remaining: 10</div>
-  </div>
   );
 };
 
