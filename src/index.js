408767d6802ade65a97dc044b6ab5b6685ecccfb
geraldgsh <geraldgsh@hotmail.com>
2020-04-05 15:32:06 +0800

    setup bahavior function

--- src/index.js
+++ src/index.js
@@ -24,18 +24,40 @@ const PlayNumber = props => (
 const StarMatch = () => {
   const [stars, setStars] = useState(utils.random(1, 9));
   const [availableNums, setAvailableNums] = useState(utils.range(1, 9));
-  const [candidateNums, setCanditateNums] = useState([]);
+  const [candidateNums, setCandidateNums] = useState([]);
 
   const candidatesAreWrong = utils.sum(candidateNums) > stars;
 
-  const numberStatus = (number) => {
+  const numberStatus = number => {
     if (!availableNums.includes(number)) {
       return 'used';
     }
     if (candidateNums.includes(number)) {
-      return candidatesAreWrong ? 'wrong': 'candidate';
+      return candidatesAreWrong ? 'wrong' : 'candidate';
     }
     return 'available';
+  };
+
+  const onNumberClick = (number, currentStatus) => {
+    if (currentStatus === 'used') {
+      return;
+    }
+
+    const newCandidateNums =
+      currentStatus === 'available'
+      ? candidateNums.concat(number)
+      : candidateNums.filter(cn => cn !== number);
+
+    if (utils.sum(newCandidateNums) !== stars) {
+      setCandidateNums(newCandidateNums);
+    } else {
+      const newAvailableNums = availableNums.filter(
+        n => !newCandidateNums.includes(n)
+      );
+      setStars(utils.randomSumIn(newAvailableNums, 9));
+      setAvailableNums(newAvailableNums);
+      setCandidateNums([]);
+    }
   };
 
   return (
@@ -45,20 +67,21 @@ const StarMatch = () => {
     </div>
     <div className="body">
       <div className="left">
-      <StarsDisplay count={stars}/>
+      <StarsDisplay count={stars} />
     </div>
     <div className="right">
-      {utils.range(1, 9).map(number =>
-        <PlayNumber 
-          key={number} 
+      {utils.range(1, 9).map(number => (
+        <PlayNumber
+          key={number}
           status={numberStatus(number)}
           number={number}
+          onClick={onNumberClick}
         />
-      )}
+      ))}
+      </div>
     </div>
-  </div>
-  <div className="timer">Time Remaining: 10</div>
-  </div>
+    <div className="timer">Time Remaining: 10</div>
+    </div>
   );
 };
 
