d392dcf65e99ee54a8f2d918a1c1ba2893fd8861
geraldgsh <geraldgsh@hotmail.com>
2020-04-05 15:29:34 +0800

    refactor stars and buttons

--- src/index.js
+++ src/index.js
@@ -3,37 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
+
 const StarMatch = () => {
+  const [stars, setStars] = useState(utils.random(1, 9));
   return (
     <div className="game">
       <div className="help">
         Pick 1 or more numbers that sum to the number of stars
-       </div>
-      <div className="body">
-        <div className="left">
-           <div className="star" />
-           <div className="star" />
-           <div className="star" />
-           <div className="star" />
-           <div className="star" />
-           <div className="star" />
-           <div className="star" />
-           <div className="star" />
-           <div className="star" />
-         </div>
-         <div className="right">
-           <button className="number">1</button>
-           <button className="number">2</button>
-           <button className="number">3</button>
-           <button className="number">4</button>
-           <button className="number">5</button>
-           <button className="number">6</button>
-           <button className="number">7</button>
-           <button className="number">8</button>
-          <button className="number">9</button>
-        </div>
       </div>
-    <div className="timer">Time Remaining: 10</div>
+    <div className="body">
+      <div className="left">
+        {utils.range(1, stars).map(starId =>
+            <div key={starId} className="star" />
+        )}
+      </div>
+      <div className="right">
+        {utils.range(1, 9).map(number =>
+          <button key={number} className="number">{number}</button>
+      )}
+    </div>
+  </div>
+  <div className="timer">Time Remaining: 10</div>
   </div>
   );
 };
